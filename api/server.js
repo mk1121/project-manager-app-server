19079bc2da304bd8eac848e8082d6c1685b05725
mk1121 <marufm.3666@gmail.com>
2023-03-06 02:54:06 +0600

    update changes
    fix read-only
    
    fix read-only issue

--- api/server.js
+++ api/server.js
@@ -1,19 +1,47 @@
-// See https://github.com/typicode/json-server#module
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const auth = require("json-server-auth");
+const jsonServer = require("json-server");
+const express = require("express");
+const http = require("http");
 
-server.use(middlewares)
-// Add this before server.use(router)
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/blog/:resource/:id/show': '/:resource/:id'
-}))
-server.use(router)
-server.listen(3000, () => {
-    console.log('JSON Server is running')
-})
+const app = express();
+const server = http.createServer(app);
+const io = require("socket.io")(server);
 
-// Export the Server API
-module.exports = server
+global.io = io;
+const fs = require("fs");
+const os = require("os");
+const path = require("path");
+fs.copyFile("db.json", os.tmpdir() + "/db.json", function (err) {
+  if (err) console.log(err);
+  else console.log("copy file succeed to" + os.tmpdir());
+});
+const router = jsonServer.router(path.resolve(os.tmpdir() + "/db.json"));
+
+// response middleware
+router.render = (req, res) => {
+
+
+    res.json(res.locals.data);
+};
+
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 9000;
+
+// Bind the router db to the app
+app.db = router.db;
+
+app.use(middlewares);
+
+const rules = auth.rewriter({
+    users: 640,
+    team: 660,
+    projects: 660,
+});
+
+app.use(rules);
+app.use(auth);
+app.use(router);
+
+server.listen(port);
+
+
